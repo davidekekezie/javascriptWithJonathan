46e1a0e876b0223d6dc72438ae0834ed7eb723d5
davidekekezie <davidekekezie33@gmail.com>
2023-06-08 17:20:42 +0100

    the last task

---
+++ Introduction/lastTask.js
@@ -0,0 +1,8 @@
+var x =4;
+var y = 5;
+var n =2;
+var a= 3;
+console.log("Question A "+""+ 2*(x+y));
+console.log("Question B "+(x%10)+x);
+console.log("Question C "+ (x**n));
+console.log("Question D "+"a/x "+(a/(a+10))+" y "+(a/(a-10))+" z "+(a/(a%10)));
